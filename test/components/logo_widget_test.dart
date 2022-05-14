04a2c467118fe4a77508f7baae6e1d07c82db42e
Rafael Kenji Nagai <rafakenji23@gmail.com>
2022-05-14 17:21:59 -0300

    fix: without label in the images

--- test/components/logo_widget_test.dart
+++ test/components/logo_widget_test.dart
@@ -4,6 +4,7 @@ import 'package:pubdev_flutter_goldentest/components/logo_widget.dart';
 void main() {
   testGoldens('Logo Widget - Golden Test', (tester) async {
     //arrange
+    await loadAppFonts();
     final builder = GoldenBuilder.column()
       ..addScenario(
         'test with flutter parameter',
